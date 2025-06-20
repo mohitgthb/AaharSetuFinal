5acf713654d3ce10dfe797b123f848d2efde3ce5
Mohit Choudhari <mohitchoudhari162@gmail.com>
2025-06-20 11:40:27 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,17 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src'),
+//     },
+//   },
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -9,5 +23,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  build: {
+    outDir: 'dist', // Make sure this is your actual build folder
+  },
+  base: '/', // Important for React Router to resolve correctly
 });
 
